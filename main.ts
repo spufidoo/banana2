ef0b71a6f6b80babe3ea4f938eef7beecd4735c1
Marcus Davage <spufidoo@users.noreply.github.com>
2023-07-05 22:06:37 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onPinPressed(TouchPin.P1, function () {
+    for (let index = 0; index < 4; index++) {
+        music.playTone(sound, music.beat(BeatFraction.Quarter))
+        sound += 25
+    }
+    sound = 440
+})
+let sound = 0
+sound = 440
